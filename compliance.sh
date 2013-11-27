2d7cfd0a0cc89d1dd38c6ed441a7925811917730
Rahul Patil <loginrahul90@gmail.com>
2013-11-27 17:45:05 +0530

    Issue #1  wrong value set: it has been solve

--- compliance.sh
+++ compliance.sh
@@ -173,7 +173,7 @@ cat <<_Params
 net.ipv4.tcp_max_syn_backlog = 4096
 net.ipv4.tcp_syncookies = 1
 net.ipv4.conf.all.rp_filter = 1
-net.ipv4.conf.all.accept_source_route = 1
+net.ipv4.conf.all.accept_source_route = 0
 net.ipv4.conf.all.secure_redirects = 0
 net.ipv4.conf.all.accept_redirects = 0
 net.ipv4.conf.default.rp_filter = 1
